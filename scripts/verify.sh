a36099a5bfc251526aea2850f87fedef22f0b929
echo <hujw77@gmail.com>
2023-11-14 11:57:51 +0800

    verify

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -2,4 +2,7 @@
 
 set -eo pipefail
 
-dapp verify-contract src/ChainLog.sol:ChainLog 0x00e600FE55158BD1426De123d0E49C69b0a03156
+deployer=0x0f14341A7f464320319025540E8Fe48Ad0fe5aec
+chainlog=0x00e600FE55158BD1426De123d0E49C69b0a03156
+
+dapp verify-contract src/ChainLog.sol:ChainLog $chainlog $deployer
