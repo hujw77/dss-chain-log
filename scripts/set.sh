1c0a662180ba9585ec1cf00ad59d6df3226f7de6
echo <hujw77@gmail.com>
2023-11-21 14:51:11 +0800

    update oracle and subapi

--- scripts/set.sh
+++ scripts/set.sh
@@ -11,7 +11,7 @@ CHANGELOG=0x000000000774019b96f067cda9fE26076649c729
 # seth send $CHANGELOG "setVersion(string)" '"1.0.0"'
 # seth send $CHANGELOG "setAddress(bytes32,address)" $(convert "CHANGELOG") $CHANGELOG
 
-path=$(dirname $0)/addr.json
+path=$(dirname $0)/conf/addr.json
 keys=$(jq -r 'keys_unsorted[]' $path)
 for k in $keys; do
   v=$(jq -r ".${k}" $path)
